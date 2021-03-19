921e1e90a27cbff346caf1dc55e76569cb08bcfe
Fred Wrodie <fred@mildlyupset.com>
2021-03-19 13:19:16 +0100

    adv

--- copy.js
+++ copy.js
@@ -1,7 +1,7 @@
 let copy = {
     round1Con:["Accept All","Accept all & visit the site","Privacy Policy","Got it!","I’m happy with this!","All right, I accept. I like cookies.","I agree to all cookies","OK!","Accept Cookies","Accept and close","I agree","This is ok","Allow","Yes I’m happy","I accept","Accept & Continue","Continue","Fine by me!"],
     round1Pro:["Customise choices","Set your cookie preferences ","Manage Cookies","Preferences","Settings","Manage Settings","No, Customise ","Learn More","Manage my cookies","Purposes","Configure Prefences","Cookie Settings","More Options","Details","Configure"],
-    tog1Con:["Create a personalised ads profile","Measure ad performance","Select personalised ads","Exact location","Avertising cookies","Legitimate Interest Marketing Cookies","Cookies set by partners for advertising, marketing, targeting and other purposes\n"],
+    tog1Con:["Create a personalised ads profile","Measure ad performance","Select personalised ads","Exact location","Advertising cookies","Legitimate Interest Marketing Cookies","Cookies set by partners for advertising, marketing, targeting and other purposes\n"],
     tog1Pro:["Do not sell my Personal Data","Disable the use of other non-essential Marketing cookies ","Disallow the use of other non-essential Marketing cookies"],
     tog1Reset:["Reset All Marketing Cookies"],
     tog2Con:["Create a personalised content profile","Select personalised content","Exact location","Experience cookies","Legitimate Interest Preferences Cookies","Social Media Cookies"],
